9731674a4df2dd5a53cbbc7c0af2f1cc9c5c7227
Zach Newell <zlantern@gmail.com>
2024-07-31 21:26:32 -0700

    Weak refs for port-to-port.

--- tests/ibnetdisc_tests.rs
+++ tests/ibnetdisc_tests.rs
@@ -1,14 +1,21 @@
-
 #[cfg(test)]
 mod tests {
-    use std::{ffi::{c_void, CStr, CString}, mem::MaybeUninit, ptr, slice};
+    use core::borrow;
+    use std::{
+        borrow::{Borrow, BorrowMut},
+        cell::RefCell,
+        ffi::{c_void, CStr, CString},
+        mem::MaybeUninit,
+        ptr,
+        rc::Rc,
+        slice,
+    };
 
     #[test]
     fn unsafe_ffi_ibnd_discover_fabric_success() {
         rsmad::umad::umad_init();
 
-
-        let ib_config = Box::new(rsmad::ibnetdisc::sys::ibnd_config_t{
+        let ib_config = Box::new(rsmad::ibnetdisc::sys::ibnd_config_t {
             max_smps: unsafe { MaybeUninit::<u32>::zeroed().assume_init() },
             show_progress: 0,
             max_hops: unsafe { MaybeUninit::<u32>::zeroed().assume_init() },
@@ -25,12 +32,14 @@ mod tests {
         let ca_name_c_str = CString::new("mlx5_0").unwrap();
         let null_ptr: *const u8 = ptr::null();
 
-        unsafe { 
-            let fabric_ptr: *mut rsmad::ibnetdisc::sys::ibnd_fabric = rsmad::ibnetdisc::sys::ibnd_discover_fabric(
-                ca_name_c_str.as_ptr() as *mut i8, 
-                1,
-                null_ptr as *mut rsmad::ibnetdisc::sys::portid,
-                ib_config_ptr);
+        unsafe {
+            let fabric_ptr: *mut rsmad::ibnetdisc::sys::ibnd_fabric =
+                rsmad::ibnetdisc::sys::ibnd_discover_fabric(
+                    ca_name_c_str.as_ptr() as *mut i8,
+                    1,
+                    null_ptr as *mut rsmad::ibnetdisc::sys::portid,
+                    ib_config_ptr,
+                );
 
             if !fabric_ptr.is_null() {
                 fabric = Some(Box::from_raw(fabric_ptr));
@@ -41,65 +50,83 @@ mod tests {
 
         let f: Box<rsmad::ibnetdisc::sys::ibnd_fabric> = fabric.unwrap();
 
-        if !f.nodes.is_null(){
-        let node = unsafe { &*f.nodes };
-        let mut next = node.next;
+        if !f.nodes.is_null() {
+            let node = unsafe { &*f.nodes };
+            let mut next = node.next;
 
-        while !next.is_null() {
+            while !next.is_null() {
+                let node: &rsmad::ibnetdisc::sys::ibnd_node = unsafe { &*next };
 
-            let node: &rsmad::ibnetdisc::sys::ibnd_node = unsafe { &*next };
+                match node.type_ {
+                    i if i == rsmad::ibmad::sys::MAD_NODE_TYPE_IB_NODE_CA as i32 => {
+                        let node_desc = unsafe { CStr::from_ptr(node.nodedesc.as_ptr()) };
 
-            match node.type_ {
-                i if i == rsmad::ibmad::sys::MAD_NODE_TYPE_IB_NODE_CA as i32 => {
-                    let node_desc = unsafe { CStr::from_ptr(node.nodedesc.as_ptr()) };
-                    
-                    let dev_id: u32 = unsafe {
-                        rsmad::ibmad::sys::mad_get_field( node.info.as_ptr() as *mut c_void, 0, rsmad::ibmad::sys::MAD_FIELDS_IB_NODE_DEVID_F)
-                    };
+                        let dev_id: u32 = unsafe {
+                            rsmad::ibmad::sys::mad_get_field(
+                                node.info.as_ptr() as *mut c_void,
+                                0,
+                                rsmad::ibmad::sys::MAD_FIELDS_IB_NODE_DEVID_F,
+                            )
+                        };
 
-                    let vendor_id: u32 = unsafe {
-                        rsmad::ibmad::sys::mad_get_field( node.info.as_ptr() as *mut c_void, 0, rsmad::ibmad::sys::MAD_FIELDS_IB_NODE_DEVID_F)
-                    };
-                    
+                        let vendor_id: u32 = unsafe {
+                            rsmad::ibmad::sys::mad_get_field(
+                                node.info.as_ptr() as *mut c_void,
+                                0,
+                                rsmad::ibmad::sys::MAD_FIELDS_IB_NODE_DEVID_F,
+                            )
+                        };
 
-                    println!("HCA NodeDesc:{:?}, GUID:{:x}, SMALID:{}, DEVID:{}, VENDORID:{}", node_desc, node.guid, node.smalid, dev_id, vendor_id);
+                        println!(
+                            "HCA NodeDesc:{:?}, GUID:{:x}, SMALID:{}, DEVID:{}, VENDORID:{}",
+                            node_desc, node.guid, node.smalid, dev_id, vendor_id
+                        );
+                    }
+                    i if i == rsmad::ibmad::sys::MAD_NODE_TYPE_IB_NODE_SWITCH as i32 => {
+                        let num_ports = node.numports;
+                        let port_ptrs: &[*mut rsmad::ibnetdisc::sys::ibnd_port] =
+                            unsafe { slice::from_raw_parts(node.ports, num_ports as usize) };
+                        for &port_ptr in port_ptrs {
+                            if !port_ptr.is_null() {
+                                let port = unsafe { &*port_ptr };
 
-                }
-                i if i == rsmad::ibmad::sys::MAD_NODE_TYPE_IB_NODE_SWITCH as i32 => {
-                    let num_ports = node.numports; 
-                    let port_ptrs: &[*mut rsmad::ibnetdisc::sys::ibnd_port] = unsafe { slice::from_raw_parts(node.ports, num_ports as usize) };
-                    for &port_ptr in port_ptrs {  
-                        if !port_ptr.is_null() {
-                            let port = unsafe { &*port_ptr };
+                                if !port.remoteport.is_null() {
+                                    let remoteport = unsafe { &*port.remoteport };
+                                    let remotenode = unsafe { &*remoteport.node };
 
-                            if !port.remoteport.is_null() {
-                                let remoteport =  unsafe { &*port.remoteport };
-                                let remotenode = unsafe { &*remoteport.node };
+                                    let nodedesc =
+                                        unsafe { CStr::from_ptr(remotenode.nodedesc.as_ptr()) };
 
-                                let nodedesc = unsafe { CStr::from_ptr(remotenode.nodedesc.as_ptr()) };
-
-                                println!("Remote Port LID: {} PortNum: {} NodeDesc:{:?} GUID:{:x} TYPE:{} SMLID:{}, BASE_LID:{}", 
+                                    println!("Remote Port LID: {} PortNum: {} NodeDesc:{:?} GUID:{:x} TYPE:{} SMLID:{}, BASE_LID:{}", 
                                     port.base_lid, port.portnum, nodedesc, remotenode.guid, remotenode.type_, remotenode.smalid,
                                         remoteport.base_lid);
-                            }   
-                            
-                            unsafe { 
-                                let phys_state = rsmad::ibmad::sys::mad_get_field( port.info.as_ptr() as *mut c_void, 0, rsmad::ibmad::sys::MAD_FIELDS_IB_PORT_PHYS_STATE_F);
-                                let logical_state = rsmad::ibmad::sys::mad_get_field( port.info.as_ptr() as *mut c_void, 0, rsmad::ibmad::sys::MAD_FIELDS_IB_PORT_STATE_F);
-                                println!("PhyState: {}, PortState: {}", phys_state, logical_state);
+                                }
 
-                            };
+                                unsafe {
+                                    let phys_state = rsmad::ibmad::sys::mad_get_field(
+                                        port.info.as_ptr() as *mut c_void,
+                                        0,
+                                        rsmad::ibmad::sys::MAD_FIELDS_IB_PORT_PHYS_STATE_F,
+                                    );
+                                    let logical_state = rsmad::ibmad::sys::mad_get_field(
+                                        port.info.as_ptr() as *mut c_void,
+                                        0,
+                                        rsmad::ibmad::sys::MAD_FIELDS_IB_PORT_STATE_F,
+                                    );
+                                    println!(
+                                        "PhyState: {}, PortState: {}",
+                                        phys_state, logical_state
+                                    );
+                                };
+                            }
                         }
                     }
+                    _ => {}
                 }
-                _ => {
 
-                }
+                next = node.next;
             }
-
-            next = node.next;
         }
-    }
         rsmad::umad::umad_done();
     }
 
@@ -110,45 +137,80 @@ mod tests {
         let mut fabric = rsmad::ibnetdisc::Fabric::new();
         let r = fabric.discover("mlx5_0");
         match r {
-            Ok(_) =>{
+            Ok(_) => {
                 println!("Discovery completed successfully");
             }
             Err(err) => {
                 println!("Discovery failed: {}", err);
-
-            }   
+            }
         }
 
-        for s in fabric.switches.iter() {
+        //18188380844304618496
+        //18188380844304618560
+        let sw = fabric.nodes.get(&18188380844304618560);
 
-            println!("Switch: {}", s.node_desc);
-            if let Some(ports) = &s.ports {
-                for po in ports.iter() {
-                    print!("\t[{:0>2}] 0x{:x}", po.number, s.guid);
-                    if let (Some(r_port), Some(r_node)) = (&po.remote_port, &po.remote_node) {
-                        print!(" - [{:0>2}] LID:{} {} {:?}", r_port.number, r_port.base_lid, r_node.node_desc, r_node.node_type );
-                    } else {
-                        print!(" - {} {}", po.logical_state, po.phys_state);
+        if let Some(switch_rc) = sw {
+            let switch_ref = RefCell::borrow_mut(switch_rc);
+            println!("{:?}", switch_ref.node_desc);
+            if let Some(port_ref) = &switch_ref.ports {
+                for p in port_ref {
+                    let local_port: std::cell::Ref<rsmad::ibnetdisc::Port> = RefCell::borrow(p);
+                    println!("LocalPort: {:?}", local_port);
+
+                    if let Some(rprp) = &local_port.remote_port {
+                        let r: Option<Rc<RefCell<rsmad::ibnetdisc::Port>>> = rprp.upgrade();
+                        if let Some(rp) = r {
+                            let rp_ref = RefCell::borrow(&rp);
+                            println!("  Remote Port: {:?}", rp_ref)
+                        }
                     }
-                    print!("\n");
+                    if let Some(remote_node) = &local_port.remote_node {
+                        let r = remote_node.upgrade();
+                        if let Some(rp) = r {
+                            let rp_ref = RefCell::borrow(&rp);
+                            println!("  Remote Node: {:?}: {:?}", rp_ref.node_desc, rp_ref.guid);
+                        } else {
+                        }
+                    }
                 }
             }
-
-        };
-
-        for c in fabric.cas.iter() {
-
-            print!("CA: 0x{:x} {} {}", c.guid, c.node_desc, c.smalid);
-            print!("\n");
-
-        };
-
-        for (r , k) in fabric.guids_lids.into_iter(){
-            println!("0x{:x} {}", r, k);
         }
 
-        for (r , k) in fabric.lids_guids.into_iter(){
-            println!("0x{:x} {}", r, k);
+        println!("Port Count: {:?}", fabric.ports.len());
+        println!("Node Count: {:?}", fabric.nodes.len());
+
+        for (_i, rc_node) in fabric.nodes.iter() {
+            let node = RefCell::borrow(&rc_node);
+            println!("Node: {}", node.node_desc);
+
+            if let Some(ports) = &node.ports {
+                for rc_port in ports {
+                    let port = RefCell::borrow(&rc_port); // No mutation needed
+
+                    print!("\t[{:0>2}] 0x{:x}", port.number, node.guid);
+                    if let (Some(weak_remote_port), Some(weak_remote_node)) =
+                        (&port.remote_port, &port.remote_node)
+                    {
+                        if let (Some(remote_port), Some(remote_node)) =
+                            (weak_remote_port.upgrade(), weak_remote_node.upgrade())
+                        {
+                            let rp = RefCell::borrow(&remote_port);
+                            let rn = RefCell::borrow(&remote_node);
+
+                            print!(
+                                " - [{:0>2}] LID:{} {} {:?}",
+                                rp.number, rp.base_lid, rn.node_desc, rn.node_type
+                            );
+                        } else {
+                            // Handle case where remote port/node no longer exists
+                            print!(" - (Remote port or node not available)");
+                        }
+                    } else {
+                        print!(" - {} {}", port.logical_state, port.phys_state);
+                    }
+                    println!(""); // Newline for each port
+                }
+            }
         }
 
         rsmad::umad::umad_done();
